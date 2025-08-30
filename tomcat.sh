ab56af1c3d39af417837d59e3094f391e037fe8c
Nitheesh862 <83998008+Nitheesh862@users.noreply.github.com>
2025-08-30 13:46:54 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,11 +1,39 @@
-amazon-linux-extras install java-openjdk11 -y
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.100/bin/apache-tomcat-9.0.100.tar.gz
-tar -zxvf apache-tomcat-9.0.100.tar.gz
-sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.100/conf/tomcat-users.xml
-sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.100/conf/tomcat-users.xml
-sed -i '58  a\<user username="Jaanu" password="Nitheesh@01" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.100/conf/tomcat-users.xml
-sed -i '59  a\</tomcat-users>' apache-tomcat-9.0.100/conf/tomcat-users.xml
-sed -i '56d' apache-tomcat-9.0.100/conf/tomcat-users.xml
-sed -i '21d' apache-tomcat-9.0.100/webapps/manager/META-INF/context.xml
-sed -i '22d'  apache-tomcat-9.0.100/webapps/manager/META-INF/context.xml
-sh apache-tomcat-9.0.100/bin/startup.sh
+#!/bin/bash
+set -e
+
+# Variables
+TOMCAT_VERSION="9.0.108"
+TOMCAT_TAR="apache-tomcat-${TOMCAT_VERSION}.tar.gz"
+DOWNLOAD_URL="https://downloads.apache.org/tomcat/tomcat-9/v${TOMCAT_VERSION}/bin/${TOMCAT_TAR}"
+USERNAME="Jaanu"
+PASSWORD="Nitheesh@01"
+
+echo "Installing Java 11 (Amazon Corretto)..."
+sudo dnf install java-11-amazon-corretto -y
+
+echo "Downloading Apache Tomcat ${TOMCAT_VERSION}..."
+wget "${DOWNLOAD_URL}"
+
+echo "Extracting Tomcat..."
+tar -zxvf "${TOMCAT_TAR}"
+
+cd "apache-tomcat-${TOMCAT_VERSION}"
+
+echo "Configuring tomcat-users.xml..."
+cp conf/tomcat-users.xml conf/tomcat-users.xml.bak
+sed -i '/<\/tomcat-users>/ i\<role rolename="manager-gui"/>' conf/tomcat-users.xml
+sed -i '/<\/tomcat-users>/ i\<role rolename="manager-script"/>' conf/tomcat-users.xml
+sed -i "/<\/tomcat-users>/ i\<user username=\"${USERNAME}\" password=\"${PASSWORD}\" roles=\"manager-gui,manager-script\"/>" conf/tomcat-users.xml
+
+echo "Enabling remote access to Manager app..."
+sed -i '21d' webapps/manager/META-INF/context.xml || true
+sed -i '22d' webapps/manager/META-INF/context.xml || true
+
+echo "Starting Tomcat..."
+sh bin/startup.sh
+
+echo
+echo "Tomcat ${TOMCAT_VERSION} installed and started successfully."
+echo "Access manager at: http://<your-server-ip>:8080/manager/html"
+echo "Username: ${USERNAME}"
+echo "Password: ${PASSWORD}"
